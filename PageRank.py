802968fe89c68e2ca2ffb4adbcd8ac6381a231e1
feljac <felixjacoby@yahoo.fr>
2018-12-12 17:15:13 +0100

    organising

--- PageRank.py
+++ PageRank.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import csv
 
 def readCsvFile(file_name):
@@ -10,21 +10,29 @@ def readCsvFile(file_name):
         return table
 
 
+"""A : np . matrix , alpha : f l o a t , v : np . array , m: bool"""
+def itemRank(a, alpha, v, m):
+    if m:
+        #récurence
+
+    else:
+        #inversion matricielle
+        
+    return 'None'
+
+
 def main():
-    adjacante_matrix = readCsvFile("Adjacante_matrice.csv")
-    print("adja ->",adjacante_matrix)
-    v = readCsvFile("Personnalisation_Student27.csv")[0]
-    print("v -> ",v)
+    a = np.array(readCsvFile("Adjacante_matrice.csv"))
+    print("a ->",a)
+    v = np.array(readCsvFile("Personnalisation_Student27.csv")[0])
+    print("v -> ", v)
+
+    x = itemRank(a, 0.15, v, True)
+    print(x)
+    x = itemRank(a, 0.15, v, True)
+    print(x)
+
     return 0
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-"""A : np . matrix , alpha : f l o a t , v : np . array , m: bool"""
-def itemRank(a, alpha, v, m):
-    return None
