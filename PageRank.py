b99a5b59c0a8fba25295217d255aa40a03ab633a
feljac <felixjacoby@yahoo.fr>
2018-12-18 20:38:39 +0100

    modif roman

--- PageRank.py
+++ PageRank.py
@@ -17,9 +17,9 @@ def prob_matrix(a):
         value = np.sum(li)
         if value != 0:
             if p is None:
-                p = np.array([np.divide(li, value)])
+                p = np.matrix(np.divide(li, value))
             else:
-                p = np.append(p, np.array([np.divide(li, value)]), axis=0)
+                p = np.append(p, np.matrix(np.divide(li, value)), axis=0)
     return p
 
 
@@ -37,18 +37,20 @@ def recur_res(p, a, x, v):
 """A : np . matrix , alpha : f l o a t , v : np . array , m: bool"""
 def itemRank(a, alpha, v, m):
     p = np.transpose(prob_matrix(a))
+    vn = np.transpose(np.divide(v, np.sum(v)))
+    vm = np.reshape(vn, (10, 1))
     if m:
         """récurence"""
-        return recur_res(p, alpha, v, v)
+        return recur_res(p, alpha, vm, vm)
     else:
         """inversion matricielle"""
-        I = np.identity(len(a[0]))
+        I = np.identity(a.__len__())
         matrix_inv = np.linalg.inv(np.subtract(I, np.multiply(alpha, p)))
-        return np.multiply(1-alpha, np.dot(matrix_inv, v))
+        return np.multiply(1-alpha, np.dot(matrix_inv, vm))
 
 
 def main():
-    a = np.array(readCsvFile("Adjacante_matrice.csv"))
+    a = np.matrix(readCsvFile("Adjacante_matrice.csv"))
     v = np.array(readCsvFile("Personnalisation_Student27.csv")[0])
     x = itemRank(a, 0.15, v, True)
     print("récurence ->", x)
